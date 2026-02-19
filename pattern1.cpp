ad3f1da918fa8c1b641edc540f03ffec3c5a18e5
manoj189404 <manoj189404@gmail.com>
2026-02-20 00:13:21 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,n,i,no;
+    cin>>n;
+    r=1;
+    while(r<=n){
+        i=1;
+        while(i<=n-r){
+            cout<<' ';
+            i=i+1;
+        }
+        i=1;
+        while(i<=r)
+    }
+} 
